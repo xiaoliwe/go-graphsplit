326e19d137eb72d891256a73c09a2eda8f740eec
OC <hi@xiaoli.dev>
2022-11-10 18:38:28 +0800

    printf records of csv

--- tpupload.go
+++ tpupload.go
@@ -56,7 +56,11 @@ func Upload(ctx context.Context, carDir, bucket string) error {
 
 	//Get the manifest.csv and read CID of file.
 	records := readCsvFile("../17GData/manifest.csv")
-	fmt.Println(records)
+	fmt.Println(len(records))
+
+	for value := range records {
+		fmt.Println(" %v\n", records[value])
+	}
 
 	//Join the car file's path , like : /disk/17GData/xxxxxx.car
 
