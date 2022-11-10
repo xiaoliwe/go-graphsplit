c9adde768ea867470c7bd3f5d79d93a351387398
OC <hi@xiaoli.dev>
2022-11-10 16:42:46 +0800

    add tpupload file

---
+++ tpupload.go
@@ -0,0 +1,26 @@
+package graphsplit
+
+import (
+	"bufio"
+	"context"
+
+	"os"
+)
+
+func fileList(file string) []string {
+	a, err := os.Open(file)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	buff := bufio.NewReader(a)
+	var ret []string
+	l, _, err := buff.ReadLine()
+	for ; err == nil; l, _, err = buff.ReadLine() {
+		ret = append(ret, string(l))
+	}
+	return ret
+}
+
+func Upload(ctx context.Context, carDir, bucket string) error {
+	return nil
+}
