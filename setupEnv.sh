56515f56e9cf3124a847d9de68875e2c15fad974
OC <hi@xiaoli.dev>
2022-11-16 11:43:27 +0800

    added wait script

--- setupEnv.sh
+++ setupEnv.sh
@@ -9,17 +9,19 @@ echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/"' >> ~
 sudo ldconfig
 
 #Setup the ~/.bashrc and user's env
-alias apt-up='apt update && apt upgrade'
+echo 'alias apt-up='apt update && apt upgrade'' >> ~/.bashrc
 
 wait $!
-
+echo 'Installing golang package....'
 #Install golang package
 wget /root/https://go.dev/dl/go1.19.3.linux-amd64.tar.gz 
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+echo 'Golang package finished !'
 
 wait $!
 
+echo 'build repos....'
 #build repos
 git submodule update --init --recursive
 make ffi && make
