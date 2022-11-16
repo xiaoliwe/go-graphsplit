54b589f68d6b5e816cef8ede3ff35899140c4b9c
OC <hi@xiaoli.dev>
2022-11-16 11:55:57 +0800

    add sleep command

--- setupEnv.sh
+++ setupEnv.sh
@@ -3,6 +3,9 @@
 #install liblwloc-dev
 sudo apt install libhwloc-dev
 
+sleep 5
+wait $!
+
 #install libIOpenCL
 sudo ln -s /usr/lib/x86_64-linux-gnu/libOpenCL.so.1 /usr/lib/libOpenCL.so
 echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/"' >> ~/.bashrc
@@ -19,6 +22,7 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 echo 'Golang package finished !'
 
+sleep 10
 wait $!
 
 echo 'build repos....'
