56e03ead9a3c0d0dcb3929507cbcc4c904203c47
OC <hi@xiaoli.dev>
2022-11-16 11:34:08 +0800

    added golang command for env

--- setupEnv.sh
+++ setupEnv.sh
@@ -11,6 +11,11 @@ sudo ldconfig
 #Setup the ~/.bashrc and user's env
 alias apt-up='apt update && apt upgrade'
 
+#Install golang package
+wget /root/https://go.dev/dl/go1.19.3.linux-amd64.tar.gz 
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+
 #build repos
 git submodule update --init --recursive
 make ffi && make
