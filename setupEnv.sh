f689d96ce0282befccb4e5b05d10710327cd25e0
OC <hi@xiaoli.dev>
2022-11-17 15:14:26 +0800

    added source comamnd

--- setupEnv.sh
+++ setupEnv.sh
@@ -5,30 +5,24 @@ sudo apt install libhwloc-dev
 
 sleep 5
 wait $!
-
 #install libIOpenCL
 sudo ln -s /usr/lib/x86_64-linux-gnu/libOpenCL.so.1 /usr/lib/libOpenCL.so
-#result=''
-if ！ command -v 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/"' &> /dev/null 
-then 
-    echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/"' >> ~/.bashrc
-    exit
-fi
-
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/"' >> ~/.bashrc
 sudo ldconfig
 
 #Setup the ~/.bashrc and user's env
-
-wait $!
 echo '++++++++ Installing golang package....++++++'
 #Install golang package
 wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz 
 
+echo '+++++++ installing golang package...+++++++'
+
 sleep 10
 wait $!
-
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc
+
 echo '+++++++ Golang package finished !+++++++'
 
 echo '+++++++++ build repos....+++++++'
@@ -40,4 +34,6 @@ wait $!
 
 make ffi && make
 
+sleep 10
+wait $!
 
