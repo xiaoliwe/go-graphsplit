67534172320abb6bdc0fb687c5724aaa509f2fe5
xiaoliwe <hi@xiaoli.dev>
2022-12-07 18:53:21 +0800

    added taskChunk file

---
+++ taskChunkUpload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#$1--path to CAR files
+#$2--path to source data
+#$3--bucket name
+
+printf "\n"
+echo "< ++++++ Starting to chunk the car files at $(date) ++++++ >"
+if ./graphsplit chunk --car-dir="$1" --slice-size=17897856102 --parallel=15 --graph-name=fildc --calc-commp=true --add-padding=false --rename=false --parent-path="$2" "$2"; then
+    now="$(date +"%m-%d-%Y")"
+    mv "$1"manifest.csv "$1""$now"-manifest.csv
+else
+    echo "chunk data has error at $(date) $?"
+fi
+printf "\n"
+echo "< ++++++ Split car files have done! at $(date) ++++++>"
+printf "\n"
+
+echo "< ++++++ Upload CAR files to storage with qshell ++++++>"
+mv "$1"* /fcfs/
+
+sleep 20
+wait $!
+printf "\n"
+echo "< ------ Congrats! All the tasks have finished! at $(date)------> "
