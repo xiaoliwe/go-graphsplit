94b2e8682d05757e880171a04186b0538baef5aa
OC <hi@xiaoli.dev>
2022-12-14 18:41:52 +0800

    remove  parameter

--- taskChunkU.sh
+++ taskChunkU.sh
@@ -5,7 +5,7 @@
 
 printf "\n"
 echo "< ++++++ Starting to chunk the car files at $(date) ++++++ >"
-if graphsplit chunk --car-dir="$1" --slice-size=18297856102 --parallel=15 --graph-name=fildc --calc-commp=true --add-padding=false --rename=false --parent-path="$2" "$2"; then
+if ./graphsplit chunk --car-dir="$1" --slice-size=18297856102 --parallel=15 --graph-name=fildc --calc-commp=true --add-padding=false --rename=false --parent-path="$2" "$2"; then
     now="$(date +"%m-%d-%Y")"
     mv "$1"manifest.csv "$1""$now"-manifest.csv
 else
